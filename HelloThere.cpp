8794287473e81b8788568b5f08a9e666889cec56
ibrahim <ibrahimmu3533@gmail.com>
2024-03-03 21:23:21 +0100

    update

--- HelloThere.cpp
+++ HelloThere.cpp
@@ -5,5 +5,32 @@ using namespace std;
 int main() {
 	cout << "hello thre " << endl;
 	cout << "game over" << endl;
+	cout << R"(
+a
+a
+a
+a
+a
+a
+a
+a
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaa
+aaaa
+
+aa
+a
+a
+a
+a
+a
+aaaaaaaaaaaaaaaa
+
+
+aaaaaaaaaaaaaa
+
+
+)";
 	cin.get();
 }
