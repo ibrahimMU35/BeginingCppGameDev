86589f1bf39d031d8d41bd5150f60d4ef8135a67
ibrahim <ibrahimmu3533@gmail.com>
2024-03-29 23:31:16 +0100

    update

---
+++ Class/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Class;
+
+class Wizard {
+    public string name { get; set; }
+    public string favoriteSpell { get; set; }
+    public int spellSlot { get; set; }
+    public float experience { get; set; }
+    public void CastSpell(){
+        System.Console.WriteLine(name + " casts " + favoriteSpell );
+        spellSlot--;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Wizard wizard01 = new Wizard();
+        wizard01.name = "Parry Hopper";
+        wizard01.favoriteSpell ="unexpecto";
+        wizard01.spellSlot = 2;
+        wizard01.experience=0.1f;
+        System.Console.WriteLine(wizard01.spellSlot);
+       wizard01.CastSpell();
+        System.Console.WriteLine(wizard01.spellSlot);
+
+
+        Console.ReadKey();
+    }
+}
